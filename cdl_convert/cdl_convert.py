52c91cd05f1f249fee08a4928ff030caebb75642
Sean Wallitsch <sean@grenadehop.com>
2015-07-04 10:44:43 -0700

    Fixes Issue 42. cdl_convert script not exporting CCs from CDL

--- cdl_convert/cdl_convert.py
+++ cdl_convert/cdl_convert.py
@@ -251,6 +251,11 @@ def main():  # pylint: disable=R0912
             if filetype_in in config.COLLECTION_FORMATS:
                 for color_correct in color_decisions.color_corrections:
                     sanity_check(color_correct)
+                for decision in color_decisions.color_decisions:
+                    if decision.is_ref:
+                        sanity_check(decision.cc.cc)
+                    else:
+                        sanity_check(decision.cc)
             else:
                 sanity_check(color_decisions)
 
@@ -260,6 +265,11 @@ def main():  # pylint: disable=R0912
                 if filetype_in in config.COLLECTION_FORMATS:
                     for color_correct in color_decisions.color_corrections:
                         write_single_file(color_correct, ext)
+                    for decision in color_decisions.color_decisions:
+                        if decision.is_ref:
+                            write_single_file(decision.cc.cc, ext)
+                        else:
+                            write_single_file(decision.cc, ext)
                 else:
                     write_single_file(color_decisions, ext)
             else:
